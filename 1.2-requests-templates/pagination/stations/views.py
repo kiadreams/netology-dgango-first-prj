02f8d1c5459c53f243d2ab3a6d636ae81ea586b3
Igor Kobelev <kia1809@yandex.ru>
2024-07-11 23:56:33 +0300

    finished task of recipes and pagination.

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,9 @@
+from csv import DictReader
+
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
+from django.conf import settings
 
 
 def index(request):
@@ -7,11 +11,15 @@ def index(request):
 
 
 def bus_stations(request):
+    with open(settings.BUS_STATION_CSV, encoding="utf-8") as csv_file:
+        stations = list(DictReader(csv_file))
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-
+    number_of_page = request.GET.get("page", 1)
+    paginator = Paginator(stations, 10)
+    page_of_stations = paginator.get_page(number_of_page)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page_of_stations,
+        'page': page_of_stations,
     }
     return render(request, 'stations/index.html', context)
