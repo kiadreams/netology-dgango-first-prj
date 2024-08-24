9c5d315509c01fbd632aaf6b620b45f75ef8d035
Igor Kobelev <kia1809@yandex.ru>
2024-08-24 22:56:02 +0300

    the m2m-relations task is finished

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,31 @@
-from django.contrib import admin
+from pprint import pprint
 
-from .models import Article
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Scope, Tag
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_sections = sum(
+            form.cleaned_data.get('is_main', 0) for form in self.forms
+        )
+        print(main_sections)
+        if not main_sections:
+            raise ValidationError('Укажите основной раздел!')
+        elif main_sections > 1:
+            raise ValidationError('Основной раздел должен быть только один!')
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
+
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -10,5 +10,16 @@ def articles_list(request):
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
+    object_list = Article.objects.order_by(ordering).all()
+    # for article in object_list:
+    #     print(dir(article.scopes))
+    #     print()
+    #     print(type(article))
+    #     for scope in article.scopes.all():
+    #         print(type(scope))
+    #         print(scope.name)
+    #     break
+
+    context['object_list'] = object_list
 
     return render(request, template, context)
